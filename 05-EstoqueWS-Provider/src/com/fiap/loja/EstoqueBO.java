83db37a90e2f0502806eea12c077da86f3ff1cd6
logonpf <logonpf@fiap.com.br>
2018-03-07 22:47:02 -0300

    WS SOAP

--- 05-EstoqueWS-Provider/src/com/fiap/loja/EstoqueBO.java
+++ 05-EstoqueWS-Provider/src/com/fiap/loja/EstoqueBO.java
@@ -1,10 +1,22 @@
 package com.fiap.loja;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.axis2.AxisFault;
 
 import com.fiap.loja.to.ProdutoTO;
 
 public class EstoqueBO {
+	
+	public List<ProdutoTO> listar(){
+		List<ProdutoTO> lista = new ArrayList<>();
+		lista.add(new ProdutoTO(401, 
+				"Camiseta Masculina", 50, "Branca"));
+		lista.add(new ProdutoTO(402, 
+				"Camiseta Feminina", 100, "Rosa"));
+		return lista;
+	}
 
 	public ProdutoTO consultarProduto(int codigo) throws AxisFault {
 		ProdutoTO to = null;
